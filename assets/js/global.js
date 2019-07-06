4596aa7794e9e25271b65e7dd6a61367c229d74b
Matt Jennings <matt@mattjennings.net>
2019-07-06 15:38:12 -0700

    added firebase connection via global.js and adding external JS script links

---
+++ assets/js/global.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQ2RB_vJkGuIjbQwfPlHC-sKc9qC1A1Uo",
+    authDomain: "fir-bootcamp-activities-83a6f.firebaseapp.com",
+    databaseURL: "https://fir-bootcamp-activities-83a6f.firebaseio.com",
+    projectId: "fir-bootcamp-activities-83a6f",
+    storageBucket: "fir-bootcamp-activities-83a6f.appspot.com",
+    messagingSenderId: "349432820365",
+    appId: "1:349432820365:web:2ecc7d4b41fadb08"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
