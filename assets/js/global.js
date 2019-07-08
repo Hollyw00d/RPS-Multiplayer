bbcac587b651e53eb0c2e16ea1db8786661475c7
Matt Jennings <matt@mattjennings.net>
2019-07-08 12:23:18 -0700

    middle of trying to get player1`.name

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
+  // Messages
+  var $messages = $('#messages');
+
   // Get player 1 Elements using jQuery
   var $player1StartGame = $('#player1StartGame');
   var $player1Name = $('#player1Name');
   var $player1NameInput = $('#player1NameInput');
-  var $player1Email = $('#player1Email');
-  var $player1Password = $('#player1Password');
-  
+
   // Get player 2 Elements using jQuery
   var $player2StartGame = $('#player2StartGame');
   var $player2Name = $('#player2Name');
   var $player2NameInput = $('#player1NameInput');
-  var $player2Email = $('#player2Email');
-  var $player2Password = $('#player2Password');
-
-  // Get player 1 and player 2 in array
-  var playersArr = [];
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
@@ -32,53 +28,93 @@ $(document).ready(function() {
   var database = firebase.database();
   var auth = firebase.auth();
 
+  var gameState = database.ref('/gameState');
+  var players = database.ref('/players');
+  var player1 = database.ref('/players/player1');
+  var player2 = database.ref('/players/player2');
+  
+  function startGame() {
+    gameState.set('true');
+
+    var state = gameState.once("value").then(function (snapshot) {
+      console.log (snapshot.val()); 
+      if (snapshot.val() === false) {    
+        // pop start modal
+        // supress join modal
+        console.log ("game is off"); 
+      }  
+      
+      else {
+        // hide start modal
+        $messages.removeClass('d-none');
+        console.log ("game is on"); 
+      }
+    }) 
+	   $("#p1chatSection").hide(); 
+    $("#p2chatSection").hide();
+  }
+  
+
   $player1StartGame.on('click', function() {   
 
     var name1 = $player1NameInput.val().toString().trim();
-    var email1 = $player1Email.val().toString().trim();
-    var pass1 = $player1Password.val();
 
-    var promiseForEmailPassword = auth.createUserWithEmailAndPassword(email1, pass1)
-      .then(function () {
-        user = firebase.auth().currentUser;
-        user.sendEmailVerification();
-      })
-      .then(function () {
-        user.updateProfile({
-          displayName: name1
-        });
-      })
-      .catch(function(error) {
-        console.log(error.message);
-      });
-
-    promiseForEmailPassword.catch(function(error) {
-      console.log(error.message);
-    });
-
-    auth.onAuthStateChanged(function(user) {
-      if(user) {
-        console.log(user.displayName);
-      }
-      else {
-        console.log('not logged in');
-      }
-    });
-
+    startGame();
+    
     $player1NameInput.val('');
-    $player1Email.val('');
-    $player1Password.val('');
+
+
+    player1.set({
+      name: name1,
+      id: 12
+    });
+
+    player1.on("child_added", function(snapshot) {
+      var player1Data = snapshot.val();
+      console.log(player1Data);
+      console.log('Blah');
+      $player1Name.text(player1Data);
+    });
+
+
+    // var promiseForEmailPassword = auth.createUserWithEmailAndPassword(email1, pass1)
+    //   .then(function () {
+    //     user = firebase.auth().currentUser;
+    //     user.sendEmailVerification();
+    //   })
+    //   .then(function () {
+    //     user.updateProfile({
+    //       displayName: name1
+    //     });
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error.message);
+    //   });
+
+    // promiseForEmailPassword.catch(function(error) {
+    //   console.log(error.message);
+    // });
+
+    // auth.onAuthStateChanged(function(user) {
+    //   if(user) {
+    //     console.log(user.displayName);
+    //   }
+    //   else {
+    //     console.log('not logged in');
+    //   }
+    // });
+
   });
 
-  auth.onAuthStateChanged(function(user) {
-    if(user) {
-      console.log(user.displayName);
-      console.log(playersArr);
-    }
-    else {
-      console.log('not logged in');
-    }
-  });
+  // auth.onAuthStateChanged(function(user) {
+  //   if(user) {
+  //     console.log(user.displayName);
+  //     console.log(playersArr);
+  //   }
+  //   else {
+  //     console.log('not logged in');
+  //   }
+  // });
   
   
 });
