67c8b918e944e6ff72ac08701d5292ed1b0244e6
Matt Jennings <matt@mattjennings.net>
2019-07-13 22:40:39 -0700

    completed train schedule project, instead of multi player paper rock scissors

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,177 +1,88 @@
 $(document).ready(function() {
-  // Messages
-  var $messages = $('#messages');
 
-  // Get player 1 Elements using jQuery
-  var $player1StartGame = $('#player1StartGame');
-  var $player1Name = $('#player1Name');
-  var $player1NameInput = $('#player1NameInput');
-  var $player1Container = $('#player1-container');
-  var $player1RpsBtn = $('.player1Btn');
-  var $player1Msg = $('#player1Msg');
-
-  // Get player 2 Elements using jQuery
-  var $player2StartGame = $('#player2StartGame');
-  var $player2Name = $('#player2Name');
-  var $player2NameInput = $('#player2NameInput');
-  var $player2Container = $('#player2-container');
-  var $player2RpsBtn = $('.player2Btn');
-  var $player2Msg = $('#player2Msg');
-
-  // Game elements using jQuery
-  $gameResult = $('#game-result');
-  $gameResultMsg = $('#game-result-msg');
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDQ2RB_vJkGuIjbQwfPlHC-sKc9qC1A1Uo",
-    authDomain: "fir-bootcamp-activities-83a6f.firebaseapp.com",
-    databaseURL: "https://fir-bootcamp-activities-83a6f.firebaseio.com",
-    projectId: "fir-bootcamp-activities-83a6f",
-    storageBucket: "fir-bootcamp-activities-83a6f.appspot.com",
-    messagingSenderId: "349432820365",
-    appId: "1:349432820365:web:2ecc7d4b41fadb08"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-
-  var gameState = database.ref('gameState');
- 
-  var player1 = database.ref('players/player1');
-  var player2 = database.ref('players/player2');
-  var players = database.ref('players');
-
-  var player1NameRef = database.ref('players/player1/name');
-  var player2NameRef = database.ref('players/player2/name');
-
-  var player1Wins = database.ref('players/player1/wins');
-  var player1WinsCount = 0;
-  var player1Loses = database.ref('players/player1/loses');
-  var player1LosesCount = 0;
-  var player1Ties = database.ref('players/player1/ties');
-  var player1TiesCount = 0;
-  var player1CurrentRPS = database.ref('players/player1/currentRPS');
-
-  var player2Wins = database.ref('players/player2/wins');
-  var player1WinsCount = 0;
-  var player2Loses = database.ref('players/player2/loses');
-  var player1LosesCount = 0;
-  var player2Ties = database.ref('players/player2/ties');
-  var player2TiesCount = 0;
-  var player2CurrentRPS = database.ref('players/player2/currentRPS');
-  
-  function startGame() {
-    gameState.set('true');
-  }
-  
-  function updatePlayer1Name() {
-  
-    player1NameRef.on("value", function(snapshot) {
-      $player1Name.text(snapshot.val());
-    });
-
-    if(player1NameRef) {
-      $messages.removeClass('d-none');
-    }
-  }
-
-  function updatePlayer2Name() {
-    
-    player2NameRef.on("value", function(snapshot) {
-      $player2Name.text(snapshot.val());
-    });
-
-    if(player2NameRef) {
-      $messages.removeClass('d-none');
-    }
-  }
-
-  $player1StartGame.on('click', function() {   
-
-    var name1 = $player1NameInput.val().toString().trim();
-
-    if(name1 !== '') {
-      startGame();
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: 'AIzaSyDQ2RB_vJkGuIjbQwfPlHC-sKc9qC1A1Uo',
+        authDomain: 'fir-bootcamp-activities-83a6f.firebaseapp.com',
+        databaseURL: 'https://fir-bootcamp-activities-83a6f.firebaseio.com',
+        projectId: 'fir-bootcamp-activities-83a6f',
+        storageBucket: 'fir-bootcamp-activities-83a6f.appspot.com',
+        messagingSenderId: '349432820365',
+        appId: '1:349432820365:web:2ecc7d4b41fadb08'
+      };
       
-      $player1NameInput.val('');
-  
-      player1.set({
-        name: name1
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      var database = firebase.database();
+
+      // Elements grabbed via jQuery
+      var $addTrain = $('#addTrain');
+      var $trainName = $('#trainName');
+      var $destination = $('#destination');
+      var $firstTrainTime = $('#firstTrainTime');
+      var $frequency = $('#frequency');
+      var $trainScheduleTableTBody = $('#trainScheduleTable > tbody');
+
+      // On click event for $addTrain
+      $addTrain.on('click', function() {
+
+        // Add input tab values into 
+        // addNewTrain object
+        var addNewTrain = {
+            trainName: $trainName.val().trim(),
+            destination: $destination.val().trim(),
+            firstTrainTime: $firstTrainTime.val().trim(),
+            frequency: $frequency.val().trim()
+        };
+
+        // Push in addNewTrain object to Firebase database
+        database.ref().push(addNewTrain);
+
+        // Clear out values
+        // from form field after form submission
+        $trainName.val('');
+        $destination.val('');
+        $firstTrainTime.val('');
+        $frequency.val('');
+
       });
-  
-      updatePlayer1Name();
-    }
 
-  });
+      // Listen to database changes and on object child
+      // do stuff
+      database.ref().on('child_added', function(childSnap) {
+        // Get trainName, destination, firstTrainTime, frequency
+        // from Firebase database
+        var trainName = childSnap.val().trainName;
+        var destination = childSnap.val().destination;
+        var firstTrainTime = childSnap.val().firstTrainTime;
+        var frequency = childSnap.val().frequency;
 
-  $player2StartGame.on('click', function() {   
+        // Get first train time, converted into hh:mm format using moment
+        var firstTimeConverted = moment(firstTrainTime, 'hh:mm');
+        // Get current time
+        var currentTime = moment();
+        // Get difference in time in minutes between time right now
+        // and current time
+        var differenceInTime = currentTime.diff(moment(firstTimeConverted), 'minutes');
+        // Get remainder of difference in time and frequency of trains 
+        // in minutes using modulo operator
+        var timeRemainder = differenceInTime % frequency;
+        // For minutes until next train, subtrain frequency
+        // minus timeRemainder
+        var minutesUntilNextTrain = frequency - timeRemainder;
+        // Add current time to time timeReminder (which is convert
+        // into minutes) and format this into military time (hh:mm)
+        var nextTrain = currentTime.add(timeRemainder, 'minutes').format('hh:mm');
 
-    var name2 = $player2NameInput.val().toString().trim();
+        // Append train schedules on tbody tag in table
+        $trainScheduleTableTBody.append(
+            '<tr><td>' + trainName + '</td>' +
+            '<td>' + destination + '</td>' +
+            '<td>' + frequency + '</td>' +
+            '<td>' + nextTrain + '</td>' +
+            '<td>' + minutesUntilNextTrain + '</td><tr>'
+        );
 
-    if(name2 !== '') {
-      startGame();
-      
-      $player2NameInput.val('');
-  
-      player2.set({
-        name: name2
       });
-  
-      updatePlayer2Name();
-    }
 
-  });
-
-  $player1Container.on('click', '.player1Btn', function() {
-    player1.update({
-      currentRPS: null
-    });
-    player1.update({
-      currentRPS: $(this).val()
-    });
-    
-  });  
-
-  $player2Container.on('click', '.player2Btn', function() {
-    player2.update({
-      currentRPS: null
-    });
-
-    player2.update({
-      currentRPS: $(this).val()
-    });
-
-    players.on("value", function(snapshot) {
-
-      var player1CurrentRPSNum = snapshot.val().player1.currentRPS;
-      var player2CurrentRPSNum = snapshot.val().player2.currentRPS;
-
-      if(player1CurrentRPSNum === player2CurrentRPSNum) {
-        player1TiesCount++;
-        $player1Msg.text('You tie!');
-        $player2Msg.text('You tie!');
-        console.log('Player 1 Ties:' + player1TiesCount);
-      }
-      else if(player1CurrentRPSNum === 'paper' && player2CurrentRPSNum === 'rock') {
-        $player1Msg.text('You Win!');
-        $player2Msg.text('You Lose!');
-      }
-      else if(player1CurrentRPSNum === 'scissors' && player2CurrentRPSNum === 'rock') {
-        $player1Msg.text('You Lose!');
-        $player2Msg.text('You Win!');
-      }
-    });   
-  });  
-
-
-
-
-  updatePlayer1Name();
-  updatePlayer2Name();
-
-  player1CurrentRPS.set(null);
-  player2CurrentRPS.set(null);
-  
 });
---
+++ assets/js/global3.js
@@ -0,0 +1,329 @@
+$(document).ready(function() {
+  // Initialize Firebase
+  // Your web app's Firebase configuration
+   var config = {
+    apiKey: 'AIzaSyDQ2RB_vJkGuIjbQwfPlHC-sKc9qC1A1Uo',
+    authDomain: 'fir-bootcamp-activities-83a6f.firebaseapp.com',
+    databaseURL: 'https://fir-bootcamp-activities-83a6f.firebaseio.com',
+    projectId: 'fir-bootcamp-activities-83a6f',
+    storageBucket: 'fir-bootcamp-activities-83a6f.appspot.com',
+    messagingSenderId: '349432820365',
+    appId: '1:349432820365:web:2ecc7d4b41fadb08'
+  };
+
+
+    firebase.initializeApp(firebaseConfig);
+
+  //var choices = ["Rock", "Paper", "Scissors"]
+  var database = firebase.database();
+  
+  var player1 = database.ref("/players/player1"); 
+  var p1wins = 0; 
+  var p1losses = 0; 
+  var p1ties = 0; 
+
+  var player2 = database.ref("/players/player2"); 
+  var p2wins = 0; 
+  var p2losses = 0;
+  var p2ties = 0; 
+
+  var gameState = database.ref("/gameState"); 
+  // var chat = database.ref(".chat"); 
+  // var connectedRef = database.ref(".info/connected"); 
+  
+  // $("#startModal").modal("show"); 
+
+  startGame();
+
+  function startGame () {
+
+    var state = gameState.once("value").then ( function (snapshot) {
+      console.log (snapshot.val()); 
+      if (snapshot.val() == false) {    
+        // pop start modal
+        $("#startModal").modal("show"); 
+        // supress join modal
+        $("#joinModal").modal("hide");
+        console.log ("game is off"); 
+      }  
+      
+      else {
+        // hide start modal
+        $("#startModal").modal("hide"); 
+        // show join modal
+        $("#joinModal").modal("show");
+        console.log ("game is on"); 
+      }
+    }) 
+    
+    $("#p1chatSection").hide(); 
+    $("#p2chatSection").hide();
+  }; 
+
+  $("#reset").on("click", function () {
+    gameOver(); 
+  })
+  
+  $("#startGame").on("click", function () {
+    resetGame(); 
+  })
+
+  function resetGame () {
+    $("#startModal").modal("toggle");
+    database.ref().set({ 
+      gameState:true,  
+    })  
+  }
+
+  $("#joinGame").on("click", function () {
+    joinGame(); 
+  })
+
+  function joinGame (){
+    $("#joinModal").modal("toggle");
+    console.log ("you're in!"); 
+  }
+
+
+  $("#addP1").on("click", function () {
+  event.preventDefault();
+  p1name = $("#name1Input").val().trim(); 
+  // $("#player1entry").empty(); 
+  // $(".addP1").hide(); 
+  // $("#player1name").text(p1name); 
+  // p1choiceGenerator(); 
+  console.log(p1name); 
+  $("#p1chatSection").show();
+  database.ref(player1).set({
+    p1name:p1name
+  })
+})
+
+$("#addP2").on("click", function () {
+  event.preventDefault();
+
+  p2name = $("#name2Input").val().trim(); 
+  // $("#player2entry").empty(); 
+  // $(".addP2").hide(); 
+  // $("#player2name").text(p2name);
+  // p2choiceGenerator(); 
+  console.log(p2name);
+  $("#p2chatSection").show(); 
+  database.ref(player2).set({
+    p2name:p2name
+  })
+
+  if (player1!=null && player2!=null) {
+    console.log ("ready to play!");
+  }
+
+})
+
+
+
+function p1choiceGenerator () {
+  //empty buttons 
+      // $("#buttons").empty(); 
+      // for loop to display answer buttons on the screen 
+      for (var i = 0; i < choices.length; i++) {
+          var a = $("<button>"); 
+          a.addClass("p1choiceButton"); 
+          a.attr("data-name", choices[i]); 
+          a.text(choices[i]); 
+          $("#p1choices").append(a);
+          console.log ("choices loaded");    
+      };
+}; 
+
+function p2choiceGenerator () {
+  //empty buttons 
+      // $("#buttons").empty(); 
+      // for loop to display answer buttons on the screen 
+      for (var i = 0; i < choices.length; i++) {
+          var a = $("<button>"); 
+          a.addClass("p2choiceButton"); 
+          a.attr("data-name", choices[i]); 
+          a.text(choices[i]); 
+          $("#p2choices").append(a);
+          console.log ("choices loaded");    
+      };
+}; 
+
+
+
+$(document).on("click", ".p1choiceButton", function() {
+  console.log("you clicked on a choice"); 
+  var p1choice = $(this).attr ("data-name"); 
+  console.log (p1choice); 
+
+  database.ref(player1).child("choice").set(p1choice); 
+
+})
+
+$(document).on("click", ".p2choiceButton", function() {
+  console.log("you clicked on a choice"); 
+  var p2choice = $(this).attr ("data-name"); 
+  console.log (p2choice); 
+
+  database.ref(player2).child("choice").set(p2choice); 
+})
+
+function ondatachange (playerSnap) {
+  console.log ("something changed");
+  var players = playerSnap.val()
+  if (!players) return;
+
+
+  if ($(".addP1").is(":visible") && (players.player1)){
+  $("#player1entry").empty();
+  $(".addP1").hide(); 
+  $("#player1name").text(p1name); 
+  $("#p2chatSection").hide();   
+  p1choiceGenerator();
+  }  
+  if ($(".addP2").is(":visible") && (players.player2)){
+    $("#player2entry").empty();
+    $(".addP2").hide(); 
+    $("#player2name").text(p2name); 
+    $("#p1chatSection").hide();
+    p2choiceGenerator();
+    }  
+  compareChoice(players);  
+  }
+  
+  gameState.on("value", function (snapshot) {
+    
+    if ((snapshot.val() == true) && ($("#startModal").is(":visible"))){
+      $("#startModal").modal("toggle"); 
+      console.log("game state change"); 
+      $("#joinModal").modal("toggle"); 
+    }
+  })
+    
+
+
+
+database.ref("players").on("value", ondatachange) 
+
+// function checkGameState () {
+//   if ((gameState = true) && ($("#startmodal").is(":visible"))){
+//     $("#startModal").modal("hide"); 
+//     $("#joinModal").modal("show"); 
+//   }
+// }
+
+// database.ref("gameState").once("value", checkGameState); 
+
+function compareChoice(players){ 
+      // console.log(players);  
+      if (!players.player1) return; 
+      if (!players.player2) return;
+      if (!players.player1.choice) return; 
+      if (!players.player2.choice) return;
+
+      if (players.player1.choice === players.player2.choice) {
+        console.log("tie"); 
+        $(".feedback").html("Tie! You both picked " + players.player1.choice + ".");
+        p1ties++; 
+        p2ties++; 
+        console.log("p1 ties" + p1ties); 
+        $("#p1ties").text(p1ties);  
+        $("#p2ties").text(p2ties);  
+        removeChoices(); 
+        testGameOver(); 
+        }
+        
+      else if 
+        ((players.player1.choice == "Rock" && players.player2.choice == "Scissors") || (players.player1.choice == "Paper" && players.player2.choice == "Rock") || (players.player1.choice == "Scissors" && players.player2.choice == "Paper")) {
+
+        console.log("not tie"); 
+        console.log("p1win");
+        p1wins++; 
+        p2losses++; 
+        $(".feedback").html("Player 1 wins.");
+        $("#p1wins").text(p1wins);  
+        $("#p2losses").text(p2losses); 
+        removeChoices(); 
+        testGameOver(); 
+        }
+
+        else {
+          console.log ("p2 wins"); 
+          p2wins++; 
+          p1losses++; 
+          console.log(p2wins); 
+          $("#p2wins").text(p2wins);  
+          $("#p1losses").text(p1losses); 
+          removeChoices();  
+          testGameOver(); 
+        }
+
+function removeChoices () {
+  database.ref("players/player1/choice").remove();
+  database.ref("players/player2/choice").remove();
+}
+  }
+
+  $("#gameOver").on("click", function () {
+    console.log ("clicked Great");
+    gameOver(); 
+  })
+
+function testGameOver () {
+  console.log ("test game Over function");
+  if ((p1wins === 3) || (p2wins === 3)) { 
+    if (p1wins == 3) {
+    $("#whoWins").text ("Player 1 is the winner!")
+    }
+    else {
+      $("#whoWins").text ("Player 2 is the winner!")
+    }
+    $("#gameOverModal").modal("show"); 
+    console.log ("gameOver"); 
+  } 
+  else return; 
+}
+
+function gameOver () {
+  console.log ("game Over function"); 
+  database.ref().set({ 
+    gameState:false,  
+  }) 
+  location.reload(); 
+}
+
+var chatHistory = database.ref("chats"); 
+
+function updateChat () {
+    chatHistory.on("child_added", function (snapshot) {
+    $("#messages").append(snapshot.val());
+    $("#messages").append("<br>");
+
+  })
+}
+
+$("#p1submitMsg").on ("click", function () {
+    // $("#messages").append("<br>"); 
+    // $("#messages").append(p1name + ": " + $("#p1message").val()); 
+    // $("#p1message").val(""); 
+    database.ref("/chats").push(p1name + ": " + $("#p1message").val()); 
+    $("#p1message").val(""); 
+    console.log ("p1 message sent"); 
+})
+
+$("#p2submitMsg").on ("click", function () {
+  // $("#messages").append("<br>");
+  // $("#messages").append(p2name + ": " + $("#p2message").val()); 
+  // $("#p2message").val(""); 
+  database.ref("/chats").push(p2name + ": " + $("#p2message").val()); 
+  $("#p2message").val(""); 
+  console.log ("p2 message sent"); 
+})
+
+updateChat(); 
+
+
+
+});
+
+
